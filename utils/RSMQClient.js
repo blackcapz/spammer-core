25034969f41b5b8c248612a8ecede67e008cc216
Igor Halfeld <igor@devstacks.com.br>
2018-11-08 17:17:21 -0200

    fix: avoid conflict with before queue

--- utils/RSMQClient.js
+++ utils/RSMQClient.js
@@ -8,4 +8,8 @@ const Q_NAME = 'SPAMMER'
 
 exports.rsmq = rsmq
 exports.Q_NAME = Q_NAME
-exports.createQueue = async () => await rsmq.createQueue({ qname: Q_NAME })
+exports.createQueue = async () => {
+  const queues = await rsmq.listQueues()
+  if (queues.includes(Q_NAME)) return
+  await rsmq.createQueue({ qname: Q_NAME })
+}
